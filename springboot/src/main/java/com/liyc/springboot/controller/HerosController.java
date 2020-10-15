47abc8b2ca082daef2b79fc0f9a094f38c2bc754
liyc712 <chargePal@163.com>
2020-10-15 15:13:17 +0800

    springboot 增加测试类

---
+++ springboot/src/main/java/com/liyc/springboot/controller/HerosController.java
@@ -0,0 +1,20 @@
+package com.liyc.springboot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author lyc
+ * @since 2020-09-22
+ */
+@RestController
+@RequestMapping("/heros")
+public class HerosController {
+
+}
