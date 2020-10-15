d2d312caae1033a9253809e45d9f3c89f0025bd9
liyc712 <chargePal@163.com>
2020-10-15 18:16:37 +0800

    目录结果修改

---
+++ common-test/src/main/java/com/liyc/common/test/CommonTest.java
@@ -0,0 +1,23 @@
+package com.liyc.common.test;
+
+import com.liyc.common.test.rpc.service.AobingService;
+import com.liyc.common.test.rpc.service.impl.AobingServiceImpl;
+import sun.security.provider.MD5;
+
+import java.lang.reflect.Method;
+
+public class CommonTest {
+
+    public static void main(String[] args) {
+        AobingService aobingService = new AobingServiceImpl();
+        Class<AobingService> serviceClass = AobingService.class;
+        try {
+            Method method = serviceClass.getMethod("hello", String.class);
+            Object tom = method.invoke(aobingService, "tom");
+            System.out.println(tom);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- shiro/src/main/java/com/liyc/shiro/utils/SaltUtils.java
+++ shiro/src/main/java/com/liyc/shiro/utils/SaltUtils.java
@@ -1,5 +1,7 @@
 package com.liyc.shiro.utils;
 
+import org.apache.shiro.crypto.hash.Md5Hash;
+
 import java.util.Random;
 
 public class SaltUtils {
@@ -22,5 +24,8 @@ public class SaltUtils {
     public static void main(String[] args) {
         String salt = getSalt(8);
         System.out.println(salt);
+        System.out.println(new Md5Hash("Action=getBaoJianInfoByBDGuidBDGuid=04fe94a1-9b80-4aac-8e38-ec45b800fd47YS6E05D968-66C6-4F8B-9433-6EB2BE056125"));
+        String a = "abcd";
+        System.out.println(a.substring(a.length()-1));
     }
 }
